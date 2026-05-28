dfc1995d39bf6dbb33571e3bc6b3cb334076852a
rafliesa <rflisaa@gmail.com>
2026-05-28 13:54:23 +0700

    Configure isolated test environment

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,6 @@ import os
 # Set environment variable palsu di sini
 # Pytest akan mengeksekusi ini sebelum meng-import module apapun dari aplikasi
 os.environ["GROQ_API_KEY_1"] = "dummy_mock_key_untuk_testing_saja"
+os.environ["FEEDBACK_SQLITE_PATH"] = "/tmp/syair_test_search_feedback.sqlite3"
+os.environ["RERANKER_PROVIDER"] = "local"
+os.environ.pop("JINA_API_KEY", None)
